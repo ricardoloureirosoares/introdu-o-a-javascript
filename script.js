32f3aab50684455f72f8a6c77f3785cc96b248ea
Ricardo Loureiro Soares <117481066+ricardoloureirosoares@users.noreply.github.com>
2023-03-09 14:56:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ console.log("Iniciando no JavaScript")
 console.log("Ricardo")
 console.log ("Ricardo " + (9+1)) //Concatenar
 console.log('Loureiro')
-//Nesse caso a crase é uma templete string
-//Template Strings são _string_s que permitem expressões embutidas. Você pode utilizar string multi-linhas e interpolação de string com elas. Basicamente é uma nova forma de criar strings e tornar o seu código um pouco mais legível
-//Com o templete string pode-se colocar o código em linhas separadas
+/*Nesse caso a crase é uma templete string
+//Template Strings são _string_s que permitem expressões embutidas. 
+Você pode utilizar string multi-linhas e interpolação de string com elas. 
+Basicamente é uma nova forma de criar strings e tornar o seu código um pouco mais legível
+Com o templete string pode-se colocar o código em linhas separadas. */
 console.log(`
 Soares
 `) 
 //Também é possivel colocar variáveis, operações expressões dentro do template string
 console.log(`Ricardo ${5+5}`)
 
-//e quando utilizar as aspas duplas e simples?
-//para coisas mais simples, quando não  houver quebra de linha, expressões, etc.
-//------------------------------
-//É possivel tilizar as aspas duplas e simples ao mesmo tempo
+/*e quando utilizar as aspas duplas e simples?
+para coisas mais simples, quando não  houver quebra de linha, expressões, etc.
+
+É possivel tilizar as aspas duplas e simples ao mesmo tempo
 console.log("Ricardo 'Loureiro' Soares")
 console.log('Ricardo "Loureiro" Soares')
-//-------------------------------------------------
+*/
 
 
 //  TIPOS DE VARIÁVEIS
@@ -39,20 +41,23 @@ console.log(-8.5 + 5.5)
 // Os números interios ou reais/float podem ser positivos ou negativos
 
 NaN // Not a Number - Não é um número - Retorno de uma operação que deu errado.
-//exemplos
+//exemplo
 console.log(10 /"a")
 
 Infinity /* infinito - Variável global, serve por exemplo para buscar números
-          dentro de uma cadeia de números pois não há nenum número maior que                  ... o infinito */
+          dentro de uma cadeia de números pois não há nenum número maior que o infinito 
+          */
 //exemplo
 console.log(Infinity)
 
 /* BOOLEAN
 
-//--True -- verdadeiro
-//--False -- falso
-// Assume somente dois valores (verdadeiro ou falso)
-//Exemplos*/
+/*
+--True -- verdadeiro
+--False -- falso
+Assume somente dois valores (verdadeiro ou falso)
+Exemplos
+*/
 console.log("a" === "a")
 console.log("a" === "b")
 
